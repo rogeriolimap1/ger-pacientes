a84a0058c1b6963cc818c9cd7d7131f0fba07e62
iure11 <iurematheus11@gmail.com>
2022-12-01 21:41:21 -0300

    2

--- ger-pacientes/src/main/java/org/iftm/pacientes/control/PacienteController.java
+++ ger-pacientes/src/main/java/org/iftm/pacientes/control/PacienteController.java
@@ -1,19 +1,46 @@
 package org.iftm.pacientes.control;
 
+import java.util.Optional;
+
 import org.iftm.pacientes.model.Paciente;
+import org.iftm.pacientes.model.repository.PacienteRepository;
+import org.springframework.data.repository.CrudRepository;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PacienteController {
 	@GetMapping("/sobre")
-	public String sobre() {return "sobre"; }
+	public String sobre() {return "sobre"; 
+	}
 	
-	  @GetMapping("/form")
-	    public String pacientesForm(Paciente paciente) {    	
-	        return "addPacienteForm";
+	@GetMapping("/form")
+	public String pacientesForm(Paciente paciente) {    	
+	return "addPacienteForm";
+	}
+	
+	  @GetMapping("form/{id}")
+	    public String updateForm(Model model, @PathVariable int id) {
+	    	Optional<Paciente> paciente = pacRepository.findById(id);
+	    	if (paciente.isPresent()) {
+	    		model.addAttribute("paciente", paciente.get());
+	    		return "atualizaPacienteForm";
+	    	}else {
+	    		return "redirect:/home";
+	    	}
 	    }
+	  
+	  @PostMapping("update/{id}")
+	    public String alterarProduto(Paciente paciente, @PathVariable int id) {
+	        pacRepository.save(paciente);
+	        return "redirect:/home";
+	    }
+
+
 	  
 	  
 }
